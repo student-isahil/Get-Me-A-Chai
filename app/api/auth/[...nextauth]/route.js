50effe8a18b5849947f18fec83c9af25976bd2c4
student-isahil <156209646+student-isahil@users.noreply.github.com>
2025-05-14 13:56:26 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,7 +12,7 @@ export const authOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
+    async signIn({ user, account, email }) {
       if (account.provider === "github") {
         await connectDb();
         const currentUser = await User.findOne({ email: email });
@@ -27,7 +27,6 @@ export const authOptions = {
       return false;
     },
 
-<<<<<<< HEAD
     async session({ session }) {
       const dbUser = await User.findOne({ email: session.user.email });
       session.user.name = dbUser.username;
@@ -38,58 +37,3 @@ export const authOptions = {
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
-=======
-export const authoptions =  NextAuth({
-    providers: [
-      // OAuth authentication providers...
-      GitHubProvider({
-        clientId: process.env.GITHUB_ID,
-        clientSecret: process.env.GITHUB_SECRET
-      }),
-    //   AppleProvider({
-    //     clientId: process.env.APPLE_ID,
-    //     clientSecret: process.env.APPLE_SECRET
-    //   }),
-    //   FacebookProvider({
-    //     clientId: process.env.FACEBOOK_ID,
-    //     clientSecret: process.env.FACEBOOK_SECRET
-    //   }),
-    //   GoogleProvider({
-    //     clientId: process.env.GOOGLE_ID,
-    //     clientSecret: process.env.GOOGLE_SECRET
-    //   }),
-    //   // Passwordless / email sign in
-    //   EmailProvider({
-    //     server: process.env.MAIL_SERVER,
-    //     from: 'NextAuth.js <no-reply@example.com>'
-    //   }),
-    ],secret: process.env.NEXTAUTH_SECRET,
-     debug: true,
-    callbacks: {
-      async signIn({ user, account, profile, email, credentials }) {
-        if(account.provider == "github") 
-        { 
-          await connectDb()
-          // Check if the user already exists in the database
-          const currentUser =  await User.findOne({email: email}) 
-          if(!currentUser){
-            // Create a new user
-              const newUser = await User.create({
-              email: user.email, 
-              username: user.email.split("@")[0], 
-            })   
-          } 
-          return true
-         }
-      },
-      
-      async session({ session, user, token }) {
-        const dbUser = await User.findOne({email: session.user.email})
-        session.user.name = dbUser.username
-        return session
-      },
-    } 
-  })
-
-  export { authoptions as GET, authoptions as POST}
->>>>>>> db18a00c028f118bf92bbf66f0b7014451a976e3
