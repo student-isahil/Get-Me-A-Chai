db18a00c028f118bf92bbf66f0b7014451a976e3
student-isahil <156209646+student-isahil@users.noreply.github.com>
2025-05-14 13:17:59 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -35,6 +35,7 @@ export const authoptions =  NextAuth({
     //     from: 'NextAuth.js <no-reply@example.com>'
     //   }),
     ],secret: process.env.NEXTAUTH_SECRET,
+     debug: true,
     callbacks: {
       async signIn({ user, account, profile, email, credentials }) {
         if(account.provider == "github") 
