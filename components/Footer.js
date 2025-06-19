c94d88ac08418211eb4514f20af3240f05a4e63c
student-isahil <156209646+student-isahil@users.noreply.github.com>
2025-06-19 17:59:27 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className='bg-gray-900 text-white flex items-center justify-center px-4 h-16'>
+    <footer className='bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800 flex items-center justify-center px-4 h-16'>
         <p className='text-center'>Copyright &copy; {currentYear} Get me A Chai - All rights reserved!</p>
     </footer>
   )
