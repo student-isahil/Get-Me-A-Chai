5e3245cfef6e00de5ac9b208b20a138e5a52065d
student-isahil <156209646+student-isahil@users.noreply.github.com>
2025-06-19 17:45:25 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,20 +6,20 @@ import Link from 'next/link'
 const Navbar = () => {
   const { data: session } = useSession()
   const [showdropdown, setShowdropdown] = useState(false)
-  const [showdropdown1, setshowdropdown1] = useState(false)
-  const timeoutRef = useRef();
+  const timeoutRef = useRef()
+
   const handleFocus = () => {
     clearTimeout(timeoutRef.current);
   };
   const handleBlur = () => {
     timeoutRef.current = setTimeout(() => {
-      showdropdown(false);
+      setShowdropdown(false);
     }, 100);
   };
 
 
   return (
-    <nav className='bg-gray-900 shadow-xl shadow-white text-white flex justify-between items-center px-4 md:h-16'>
+    <nav className='bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800 flex justify-between items-center px-4 md:h-16'>
 
       <Link className="logo font-bold text-lg flex justify-center items-center" href={"/"}>
         <img className='invertImg' src="https://res.cloudinary.com/darj9wemb/image/upload/v1747129841/tea_c3w6at.gif" width={44} alt="" />
@@ -29,26 +29,26 @@ const Navbar = () => {
 
 
       <div className='relative flex justify-center items-center   gap-1'>
-            {!session && <>
-          <button onClick={() => setshowdropdown1(!showdropdown1)} onBlur={handleBlur} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white mx-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Projects<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-            </svg>
+        {!session && <>
+          <button onClick={() => setShowdropdown(!showdropdown)} onBlur={handleBlur} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white mx-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Projects<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
+          </svg>
           </button>
 
-          <div id="dropdown" onFocus={handleFocus} className={`z-10 ${showdropdown1 ? "" : "hidden"} absolute left-[15px] top-12 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
+          <div id="dropdown" onFocus={handleFocus} className={`z-10 ${showdropdown ? "" : "hidden"} absolute left-[15px] top-12 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700`}>
             <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
               <li>
-                <Link onClick={() => setshowdropdown1(false)} href={`https://student-isahil.github.io/spotify-clone/`}  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Spotify Clone</Link>
+                <Link onClick={() => setShowdropdown(false)} href={`https://student-isahil.github.io/spotify-clone/`} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Spotify Clone</Link>
               </li>
               <li>
-                <Link onClick={() => setshowdropdown1(false)}   href="/passop"
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Password Manager</Link>
-              </li> 
+                <Link onClick={() => setShowdropdown(false)} href="/passop"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Password Manager</Link>
+              </li>
             </ul>
           </div></>
         }
         {session && <>
-          <button onClick={() => setShowdropdown(!showdropdown)} onBlur={handleBlur}  id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white mx-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Account<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+          <button onClick={() => setShowdropdown(!showdropdown)} onBlur={handleBlur} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white mx-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Account<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
             </svg>
           </button>
@@ -59,11 +59,14 @@ const Navbar = () => {
                 <Link onClick={() => setShowdropdown(false)} href="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
               </li>
               <li>
-                <Link onClick={() => setShowdropdown(false)}  href={`/${session.user.name}`} 
-                className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Your Page</Link>
+                <Link href={`/${session.user.name}`} onClick={() => setShowdropdown(false)}
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Your Page</Link>
               </li>
-        
-              
+              <li>
+                <button onClick={() => signOut({ callbackUrl: '/' })} className=" px-4 py-2 w-full text-start
+                     hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</button>
+              </li>
+
             </ul>
           </div></>
         }
