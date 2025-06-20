c6ce1d548597a0ea2d2aef86cfaced730a579a93
student-isahil <156209646+student-isahil@users.noreply.github.com>
2025-06-20 13:33:02 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,18 +1,7 @@
 
 import Dashboard from '@/components/Dashboard'
-// import { useRouter } from 'next/router'
 
 const DashboardPage = () => {
-
-<<<<<<< HEAD
-    // const {data: session} = useSession()
-    // if(!session)
-    // {
-    //     const router = useRouter()
-    //     router.push('/login')
-    // }
-=======
->>>>>>> bd37a0a (Updated full project with latest changes)
     
     return (
         <Dashboard/>
