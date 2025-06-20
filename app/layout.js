f02fc82395ae9ebd8809cd50ea68148df6f8ab6e
student-isahil <156209646+student-isahil@users.noreply.github.com>
2025-06-20 13:41:51 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
 import Script from 'next/script'
-=======
->>>>>>> bd37a0a (Updated full project with latest changes)
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-<<<<<<< HEAD
-import SessionWrapper  from "@/components/SessionWrapper";
-=======
 import SessionWrapper from "@/components/SessionWrapper";
-import Script from "next/script";
->>>>>>> bd37a0a (Updated full project with latest changes)
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -38,18 +25,10 @@ export default function RootLayout({ children }) {
           <Navbar />
           <div className=" min-h-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white">
             {children}
-<<<<<<< HEAD
-
-          </div>
-          <Footer />
-        </SessionWrapper>
-              <Script src="https://cdn.lordicon.com/lordicon.js" strategy="lazyOnload" />
-=======
           </div>
           <Footer />
         </SessionWrapper>
         <Script src="https://cdn.lordicon.com/lordicon.js"></Script>
->>>>>>> bd37a0a (Updated full project with latest changes)
       </body>
     </html>
   );
