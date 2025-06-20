7d6e57685333dd6af3d7a056762e5e7aac7779c2
student-isahil <156209646+student-isahil@users.noreply.github.com>
2025-06-20 13:30:27 +0530

    Update useractions.js

--- actions/useractions.js
+++ actions/useractions.js
@@ -1,38 +1,34 @@
-"use server"  
-// 2:15
+"use server"
 
 import Razorpay from "razorpay"
 import Payment from "@/models/Payment"
 import connectDb from "@/db/connectDb"
 import User from "@/models/User"
 
-
 export const initiate = async (amount, to_username, paymentform) => {
     await connectDb()
-    // fetch the secret of the user who is getting the payment 
-    let user = await User.findOne({username: to_username})
+    let user = await User.findOne({ username: to_username })
     const secret = user.razorpaysecret
 
     var instance = new Razorpay({ key_id: user.razorpayid, key_secret: secret })
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> bd37a0a (Updated full project with latest changes)
     let options = {
         amount: Number.parseInt(amount),
-        currency: "INR",
+        currency: "INR"
     }
+
     let x = await instance.orders.create(options)
 
-    // create a payment object which shows a pending payment in the database
-    await Payment.create({ oid: x.id, amount: amount/100, to_user: to_username, name: paymentform.name, message: paymentform.message })
+    await Payment.create({
+        oid: x.id,
+        amount: amount / 100,
+        to_user: to_username,
+        name: paymentform.name,
+        message: paymentform.message
+    })
 
     return x
-
 }
-
 
 export const fetchuser = async (username) => {
     await connectDb()
@@ -43,38 +39,25 @@ export const fetchuser = async (username) => {
 
 export const fetchpayments = async (username) => {
     await connectDb()
-    // find all payments sorted by decreasing order of amount and flatten object ids
-    let p = await Payment.find({ to_user: username, done:true }).sort({ amount: -1 }).limit(10).lean()
+    let p = await Payment.find({ to_user: username, done: true })
+        .sort({ amount: -1 })
+        .limit(10)
+        .lean()
     return p
-
 }
 
 export const updateProfile = async (data, oldusername) => {
     await connectDb()
     let ndata = Object.fromEntries(data)
 
-    // If the username is being updated, check if username is available
     if (oldusername !== ndata.username) {
         let u = await User.findOne({ username: ndata.username })
         if (u) {
             return { error: "Username already exists" }
-        }   
-        await User.updateOne({email: ndata.email}, ndata)
-        // Now update all the usernames in the Payments table 
-        await Payment.updateMany({to_user: oldusername}, {to_user: ndata.username})
-        
+        }
+        await User.updateOne({ email: ndata.email }, ndata)
+        await Payment.updateMany({ to_user: oldusername }, { to_user: ndata.username })
+    } else {
+        await User.updateOne({ email: ndata.email }, ndata)
     }
-    else{
-<<<<<<< HEAD
-
-        
-        await User.updateOne({email: ndata.email}, ndata)
-    }
-
-
-=======
-        await User.updateOne({email: ndata.email}, ndata)
-    }
->>>>>>> bd37a0a (Updated full project with latest changes)
 }
-
