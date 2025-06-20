30cc2cbda397b3aca0ee9fe5757b041782055827
student-isahil <156209646+student-isahil@users.noreply.github.com>
2025-06-20 14:58:38 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className=`${geistSans.variable} ${geistMono.variable} antialiased bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white`>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white`}>
         <SessionWrapper> 
           <Navbar />
           <div className=" min-h-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white">
